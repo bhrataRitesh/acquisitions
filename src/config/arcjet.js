e1b5c21a70cd55461a275922915aae4659d35d3d
bhrataRitesh <ratohikumar@gmail.com>
2025-12-20 21:41:41 +0530

    secure api with arcjet

---
+++ src/config/arcjet.js
@@ -0,0 +1,35 @@
+import arcjet, { shield, detectBot, slidingWindow } from '@arcjet/node';
+// import { isSpoofedBot } from '@arcjet/inspect';
+
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: [
+        'CATEGORY:SEARCH_ENGINE', 
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        'CATEGORY:PREVIEW', // Link previews e.g. Slack, Discord
+      ],
+    }),
+    slidingWindow({
+      mode: 'LIVE',
+      interval: '2s',
+      max:5                
+    })
+    // Create a token bucket rate limit. Other algorithms are supported.
+    // tokenBucket({
+    //     mode: "LIVE",
+    //     // Tracked by IP address by default, but this can be customized
+    //     // See https://docs.arcjet.com/fingerprints
+    //     //characteristics: ["ip.src"],
+    //     refillRate: 5, // Refill 5 tokens per interval
+    //     interval: 10, // Refill every 10 seconds
+    //     capacity: 10, // Bucket capacity of 10 tokens
+    // }),
+  ],
+});
+
+export default aj;
